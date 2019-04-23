58900779f1f7d0f2bc1cf2d4951af3c78362c7fb
Spencer Judge <spencer@xand.me>
2019-04-22 23:14:21 -0700

    Actually kinda doing something

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,38 @@
 #![feature(async_await, await_macro, futures_api)]
 
 use failure::Error;
-use futures::compat::Future01CompatExt;
 use futures01::Future as Future01;
-use runtime::net::UdpSocket;
+use runtime::net::{TcpListener, UdpSocket};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::time::{Duration, Instant};
 use tokio::timer::Delay;
+use bincode::serialize;
+use runtime::spawn;
 
 #[runtime::main]
 async fn main() -> Result<(), Error> {
-    let socket = UdpSocket::bind("127.0.0.1:42444")?;
-    socket.set_broadcast(true)?;
-    println!("Listening on {}", socket.local_addr()?);
-    await!(serve(socket))?;
+    await!(serve())?;
     Ok(())
 }
 
-async fn serve(mut socket: UdpSocket) -> Result<(), Error> {
-    let broadcast_addr = SocketAddr::new(
-        IpAddr::V4(Ipv4Addr::new(255, 255, 255, 255)),
-        42444,
-    );
-    // Discover peer
+async fn serve() -> Result<(), Error> {
+    let tcp_sock = TcpListener::bind("127.0.0.1:0")?;
+    let tcp_port = tcp_sock.local_addr()?.port();
+
+    let mut socket = UdpSocket::bind("127.0.0.1:42444")?;
+    socket.set_broadcast(true)?;
+    println!("Listening on {}", socket.local_addr()?);
+
+    spawn(file_srv(tcp_sock));
+
+    // Wait for broadcast from peer
     let mut buf = vec![0u8; 100];
     loop {
-        await!(socket.send_to(b"I'm here!", broadcast_addr))?;
-        dbg!("Sent!");
         let (recv, peer) = await!(socket.recv_from(&mut buf)).unwrap();
-        println!("Got {:?} from {}", &buf, &peer);
+        println!("Got {} bytes {:?} from {}", &recv, &buf, &peer);
+        // Reply with tcp portnum
+        let portnum = serialize(&tcp_port)?;
+        await!(socket.send_to(&portnum, &peer))?;
 
         let sleep = Delay::new(Instant::now() + Duration::from_millis(300))
             .map_err(|e| panic!("timer failed; err={:?}", e));
@@ -36,27 +40,28 @@ async fn serve(mut socket: UdpSocket) -> Result<(), Error> {
     }
 }
 
+async fn file_srv(mut tcp_sock: TcpListener) -> Result<(), Error> {
+    println!("TCP listening on {}", tcp_sock.local_addr()?);
+    Ok(())
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
-    use runtime::spawn;
+    use bincode::deserialize;
 
     #[runtime::test]
     async fn peers_discover_each_other() {
-        let sock1 = UdpSocket::bind("127.0.0.1:42444").unwrap();
-        sock1.set_broadcast(true).unwrap();
-        spawn(serve(sock1));
+        spawn(serve());
 
-        let broadcast_addr = SocketAddr::new(
-            IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-            42444,
-            //        socket.local_addr()?.port(),
-        );
+        let broadcast_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 42444);
         let mut client_s = UdpSocket::bind("127.0.0.1:0").unwrap();
         client_s.set_broadcast(true).unwrap();
-        let mut buf = vec![0u8; 1024];
+        let mut buf = vec![0u8; 24];
         await!(client_s.send_to(b"I'm a client!", broadcast_addr)).unwrap();
         dbg!("ClientSent!");
-        let (recv, peer) = await!(client_s.recv_from(&mut buf)).unwrap();
+        await!(client_s.recv_from(&mut buf)).unwrap();
+        let tcp_port: u16 = deserialize(&buf).unwrap();
+        println!("Client found server tcp port {}", &tcp_port);
     }
 }
