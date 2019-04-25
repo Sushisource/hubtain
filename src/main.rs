a3406f301fd00b03fbf93a923e251c2d5116957e
Spencer Judge <spencer@xand.me>
2019-04-24 22:10:19 -0700

    Exctract client

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-#![feature(async_await, await_macro, futures_api)]
+#![feature(async_await, await_macro)]
+
+mod client;
 
 use bincode::serialize;
 use failure::Error;
@@ -9,7 +11,6 @@ use runtime::{
     spawn,
 };
 use std::{
-    net::{IpAddr, Ipv4Addr, SocketAddr},
     time::{Duration, Instant},
 };
 use tokio::timer::Delay;
@@ -39,8 +40,8 @@ async fn serve<T: 'static + AsyncRead + Send + Unpin>(
     // Wait for broadcast from peer
     let mut buf = vec![0u8; 100];
     loop {
-        let (recv, peer) = await!(socket.recv_from(&mut buf)).unwrap();
-        println!("Got {} bytes {:?} from {}", &recv, &buf, &peer);
+        let (_, peer) = await!(socket.recv_from(&mut buf)).unwrap();
+        println!("Got client handshake from {}", &peer);
         // Reply with tcp portnum
         let portnum = serialize(&tcp_port)?;
         await!(socket.send_to(&portnum, &peer))?;
@@ -65,31 +66,17 @@ async fn data_srv<T: AsyncRead + Unpin>(
 #[cfg(test)]
 mod test {
     use super::*;
-    use bincode::deserialize;
-    use runtime::net::tcp::TcpStream;
+    use crate::client::DownloadClient;
 
     #[runtime::test]
-    async fn peers_discover_each_other() {
+    async fn basic_transfer() {
         let tcp_sock = TcpListener::bind("127.0.0.1:0").unwrap();
         let udp_sock = UdpSocket::bind("127.0.0.1:0").unwrap();
         let udp_port = udp_sock.local_addr().unwrap().port();
         spawn(serve(tcp_sock, udp_sock, TEST_DATA));
 
-        let broadcast_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), udp_port);
-        let mut client_s = UdpSocket::bind("127.0.0.1:0").unwrap();
-        client_s.set_broadcast(true).unwrap();
-        let mut buf = vec![0u8; 24];
-        await!(client_s.send_to(b"I'm a client!", broadcast_addr)).unwrap();
-        dbg!("ClientSent!");
-        let (_, peer) = await!(client_s.recv_from(&mut buf)).unwrap();
-        let tcp_port: u16 = deserialize(&buf).unwrap();
-        println!("Client found server tcp port {}", &tcp_port);
-        let mut tcp_sock_addr = peer;
-        tcp_sock_addr.set_port(tcp_port);
-        // Connect to the tcp port
-        let mut stream = await!(TcpStream::connect(tcp_sock_addr)).unwrap();
-        let mut download = Vec::with_capacity(2056);
-        await!(stream.read_to_end(&mut download)).unwrap();
-        assert_eq!(TEST_DATA, download.as_slice());
+        let mut client = await!(DownloadClient::connect(udp_port)).unwrap();
+        let content = await!(client.download_to_vec()).unwrap();
+        assert_eq!(content, TEST_DATA);
     }
 }
