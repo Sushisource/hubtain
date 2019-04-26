ab4f2025d27c123e10937be6e8b648863edc9899
Spencer Judge <spencer@xand.me>
2019-04-25 22:05:29 -0700

    Multiple connections works

--- src/filereader.rs
+++ src/filereader.rs
@@ -2,18 +2,29 @@ use failure::Error;
 use futures::io::AsyncRead;
 use std::fs::File;
 use std::io::Read;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
 pub struct AsyncFileReader {
     file: File,
+    orig_path: PathBuf,
+}
+
+impl Clone for AsyncFileReader {
+    fn clone(&self) -> Self {
+        AsyncFileReader {
+            file: File::open(self.orig_path.as_path()).expect("Bwaaargh file open explosion"),
+            orig_path: self.orig_path.clone(),
+        }
+    }
 }
 
 impl AsyncFileReader {
     pub fn new<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
         Ok(AsyncFileReader {
-            file: File::open(path)?,
+            file: File::open(&path)?,
+            orig_path: path.as_ref().to_path_buf(),
         })
     }
 }
